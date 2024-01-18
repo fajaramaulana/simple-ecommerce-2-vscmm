b894bde9a617e4751503ff51a11bc66177716333
fajaramaulana <fajaragusmaulana@gmail.com>
2024-01-19 00:16:00 +0700

    Update email configuration and add RegisterMail class and view

---
+++ resources/views/emails/register.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Hi this message is auto generated</title>
+</head>
+
+<body>
+    <h1>Hello, {{ $name }}!</h1>
+    <p>Aku lagi ngerjain test terus aku ngirimnya otomatis yang ini. tenang aman kok email kamu gak kenapa kenapa.</p>
+</body>
+
+</html>

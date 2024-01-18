ee252ff71b44c421e16937acfbad86ce3a429137
fajaramaulana <fajaragusmaulana@gmail.com>
2024-01-18 21:16:01 +0700

    first commit

---
+++ database/migrations/2024_01_18_140950_craete_product_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_name', 100);
+            $table->integer('product_price', 100);
+            $table->string('product_image', 100);
+            $table->integer('status', 1);
+            $table->timestamps();
+        });
+    }
+
+     /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};

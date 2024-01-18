5faee43aefb83cf76ca6f8124023919a94e9e947
fajaramaulana <fajaragusmaulana@gmail.com>
2024-01-18 22:22:46 +0700

    Add phone field to users table and update UserSeeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,6 +24,7 @@ return new class extends Migration
                     'user',
                 ]
             )->default('user');
+            $table->string('phone')->nullable();
             $table->integer('status')->default(1);
             $table->timestamps();
         });
